a74f1ace3a36f78b1eacc80c108dc2a11c8243a8
Baon2024 <Baon2020@protonmail.com>
2025-08-11 16:36:38 +0100

    added new suavase file

--- conversational-ai-twilio/supabase.js
+++ conversational-ai-twilio/supabase.js
@@ -4,3 +4,8 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+const supabaseUrl2 = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey2 = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+export const supabaseReal = createClient(supabaseUrl2, supabaseAnonKey2)
