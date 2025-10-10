9e9f9b748412ebdebc8af1218f49fe5ca05f7c83
Baon2024 <Baon2020@protonmail.com>
2025-10-10 15:06:32 +0100

    forgot to add new function file

---
+++ conversational-ai-twilio/arabicTranslator.js
@@ -0,0 +1,34 @@
+import OpenAI from 'openai';
+import dotenv from 'dotenv';
+import { response } from 'express';
+
+dotenv.config()
+
+const client = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY, // Securely load API key
+});
+
+let example = 'Hello, this is Mark. I’m calling from Luxury Dubai Real Estate. How are you today?'
+
+export default async function getArabicVersion(arabicToTranslate) {
+
+  const response = await client.responses.create({
+  model: "gpt-4.1",
+  input: [
+    { role: "system", content: "translate the english text you are given into standard Arabic" },
+    { role: "user", content: arabicToTranslate }
+  ]
+});
+
+console.log(response.output[0].content[0].text);
+let output = response.output[0].content[0].text.trim()
+if (typeof output !== "string") {
+    console.log("arabicTranslator did not return a string, so I will now convert to a string")
+    output = output.toString()
+}
+
+return output
+
+}
+
+//getArabicVersion(example)
